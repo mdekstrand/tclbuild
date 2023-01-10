e2439e4024f8be6f9812a7ee2c5c4af9c93b3456
Michael Ekstrand <md@ekstrandom.net>
2023-01-10 10:23:22 -0700

    bump profile build number for manifest info

--- profiles.tcl
+++ profiles.tcl
@@ -1,8 +1,8 @@
 # Specification of build profiles
 
-profile -n 1 -stack jim custom {
+profile -n 2 -stack jim custom {
     -config-arg --without-ext=default
     -config-arg --with-ext=aio,array,exec,file,glob,namespace,package,readdir,regexp,tclcompat
 }
 
-profile -n 1 -stack jim default {}
+profile -n 2 -stack jim default {}
