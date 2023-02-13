e9d904ed16d3cbe8a3620fa8f316c2cea2586413
Michael Ekstrand <md@ekstrandom.net>
2023-02-12 21:29:01 -0700

    Squashed 'common/' changes from 8700495..ae7d552
    ae7d552 git fetch
    6452379 script to ease git updates
    42f8ffe add setops lib
    59fabc3 working fsmirror
    9334272 enable macos check
    d857e8e implement and test fs walking
    ffa9d5a document shift functions
    5e9f324 optimizations in shift/unshift
    c9fa8fc tests for the list utilities
    0d74f7f add support to make color conditional
    bd8df18 support displaying tasks and descriptions
    ebb3bcb Support multiple keys in kvlookup
    b5e1cef update pkg index
    8fcfc53 fix dep
    045f9ed better runner logging
    f7db106 super basic task runner
    
    git-subtree-dir: common
    git-subtree-split: ae7d5523155f6ce67e466ca399f9efdc4910b9e1

---
+++ fswalk.tcl
@@ -0,0 +1,40 @@
+#!/usr/bin/env jimsh
+#
+# Test program for fsextra's walk
+
+set root [file dirname [info script]]
+set auto_path [list $root/lib {*}$auto_path]
+
+package require logging
+package require getopt
+package require fsextra
+
+set walk_args [list]
+
+getopt arg $argv {
+    -v - --verbose {
+        # increase logging verbosity
+        logging::configure -verbose
+    }
+
+    --dirs-last {
+        # print directories after their contents
+        lappend walk_args -dirs-last
+    }
+    --no-dirs {
+        # print directories after their contents
+        lappend walk_args -no-dirs
+    }
+    --relative {
+        # print paths relative to root
+        lappend walk_args -relative
+    }
+
+    arglist {
+        set paths $arg
+    }
+}
+
+fswalk {*}$walk_args path $paths {
+    msg "visted $path"
+}
