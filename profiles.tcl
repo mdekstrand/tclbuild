dbe9a3baaf1fc9d1a106cf0e09f64528c26dc0f9
Michael Ekstrand <md@ekstrandom.net>
2023-03-05 09:57:01 -0700

    Update Jim and enable ensemble

--- profiles.tcl
+++ profiles.tcl
@@ -1,15 +1,15 @@
 # Specification of build profiles
 
-profile -n 6 -stack jim custom {
+profile -n 1 -stack jim custom {
     -config-arg --disable-lineedit
     -config-arg --without-ext=default
-    -config-arg --with-ext=aio,array,exec,file,glob,interp,json,namespace,oo,package,readdir,regexp,tclcompat
+    -config-arg --with-ext=aio,array,ensemble,exec,file,glob,interp,json,namespace,oo,package,readdir,regexp,tclcompat
     -post strip
 }
 
-profile -n 2 -stack jim default {}
+profile -n 1 -stack jim default {}
 
-profile -n 2 -stack jim full {
+profile -n 1 -stack jim full {
     -config-arg --full
 }
 
