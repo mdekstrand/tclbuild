eff44af91310ca3c617c190462e0f283b714d9e6
Michael Ekstrand <md@ekstrandom.net>
2023-03-05 10:18:01 -0700

    Squashed 'common/' changes from ae7d552..4af6bc0
    4af6bc0 fix kvlookup test
    cf7c1c7 help docs for git-update
    42ebe5e move read_file to fsextra
    f551ffd fcopy: better logging & return value
    b506cbe tagline normalization  & tests
    4abcffc fix missing quote in task runner
    051bbc7 Merge commit 'e5e0b8ac96c708f868ee9dc960a7a8ef40c5d71d'
    fbd99f7 better sysinfo
    452e856 make sysinfo executable
    642a7f8 add glibc detection
    7e4c7a8 add plat info & distro
    3a4de79 add ansi::wrap command
    3ed6a6c log total time
    
    git-subtree-dir: common
    git-subtree-split: 4af6bc0bfa0a4fdc5fce8d0d3aa99dcca24bb088

---
+++ sysinfo.tcl
@@ -0,0 +1,48 @@
+#!/usr/bin/env jimsh
+set root [file dirname [string map {\\ /} [info script]]]
+set auto_path [list "$root/lib" {*}$auto_path]
+
+package require logging
+package require getopt
+package require ansifmt
+package require platinfo
+
+proc display {label args} {
+    ansi::with_out stdout {
+        puts "$label: [ansi::wrap -bold {*}$args]"
+    }
+}
+
+getopt arg $argv {
+    -v - --verbose {
+        # increase logging verbosity
+        logging::configure -verbose
+    }
+    -q - --quiet {
+        # suppress informational messages
+        logging::configure -quiet
+    }
+    arglist {}
+}
+
+msg "reading TCL platform data"
+foreach key [lsort [array names tcl_platform]] {
+    display $key $tcl_platform($key)
+}
+
+msg "reading platform detection results"
+display tag [plat::tag]
+display arch [plat::arch]
+display os [plat::os]
+display flavor [plat::flavor]
+display distro [plat::distro]
+display libc [plat::libc]
+
+msg "testing platform predicates"
+foreach pred {windows unix mac musl glibc} {
+    set response {
+        {-reset no}
+        {-fg green yes}
+    }
+    display "$pred?" {*}[lindex $response [plat::is $pred]]
+}
